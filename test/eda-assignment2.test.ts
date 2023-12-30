d6334a4c4e9c7c33db07252a35cbe98365aa3479
Corey <coreyshanahan01@gmail.com>
2023-12-30 04:26:13 +0000

    Initial commit

---
+++ test/eda-assignment2.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as EdaAssignment2 from '../lib/eda-assignment2-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/eda-assignment2-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new EdaAssignment2.EdaAssignment2Stack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
